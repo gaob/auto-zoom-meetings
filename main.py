72a4eaf7b58a9e6a3c76261528e28b513f82669f
nightfir <nightfir@mail.com>
2020-06-28 14:52:20 -0400

    Remove unused import

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-import selenium.common.exceptions
 import time, datetime, platform
 import getpass
 import subprocess
